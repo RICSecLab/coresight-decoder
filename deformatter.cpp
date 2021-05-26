7345b1484e52391a6b3d8baef4e493f16b9c2b57
Yuichi Sugiyama <mmxsrup@gmail.com>
2021-05-26 18:48:20 +0900

    Add comments about deformatter

--- deformatter.cpp
+++ deformatter.cpp
@@ -3,6 +3,15 @@
 
 #include "deformatter.hpp"
 
+
+// フォーマッタはトレースIDをトレースデータに埋め込むために使用される。
+// そのため、フォーマッタに従い、指定されたトレースIDのトレースデータのみを
+// 取り出す必要がある。
+//
+// 参考: ARM CoreSight Architecture Specification v3.0 - Chapter D4 Trace Formatter
+// https://developer.arm.com/documentation/ihi0029/e
+//
+// TODO: 必要なIDだけ取り出す
 std::vector<uint8_t> deformatTraceData(const std::vector<uint8_t>& data) {
     std::vector<uint8_t> deformat_data;
     for (size_t data_idx = 0; data_idx < data.size(); data_idx += 16) {
