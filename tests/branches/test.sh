5fa48d3659836af47c0cba70412ae6a51ed8bab1
Yuichi Sugiyama <mmxsrup@gmail.com>
2021-06-14 00:27:19 +0000

    tests: Add test code to compare bitmaps

--- tests/branches/test.sh
+++ tests/branches/test.sh
@@ -5,6 +5,8 @@
 PROGRAM=../../processor
 # Suffix of the file that outputs edge coverage
 OUTPUT_FILE_SUFFIX="_edge_coverage.out"
+# Suffix of the file that outputs bitmap of edge coverage
+OUTPUT_BITMAP_FILE_SUFFIX="_bitmap.out"
 
 
 # Calculate edge coverage and output to a file
@@ -13,7 +15,13 @@ run () {
     output_file=$target$OUTPUT_FILE_SUFFIX
     lower_address="$2"
     upper_address="$3"
-    $PROGRAM $(cat $target/decoderargs.txt) --address-range=$lower_address,$upper_address > $output_file
+    bitmap_file=$target$OUTPUT_BITMAP_FILE_SUFFIX
+
+    $PROGRAM $(cat $target/decoderargs.txt) --address-range=$lower_address,$upper_address \
+                                            --bitmap-mode \
+                                            --bitmap-size=0x1000 \
+                                            --bitmap-filename=$bitmap_file \
+                                            > $output_file
 }
 
 
@@ -25,6 +33,23 @@ assert() {
     output_file2=$target2$OUTPUT_FILE_SUFFIX
 
     diff $output_file1 $output_file2
+    result="$?"
+    if [ $result -ne 0 ]; then
+        echo "Found differences: $target1, $target2"
+        exit 1
+    fi
+}
+
+
+# Compare bitmap
+assert_bitmap() {
+    target1="$1"
+    target2="$2"
+    output_file1=$target1$OUTPUT_BITMAP_FILE_SUFFIX
+    output_file2=$target2$OUTPUT_BITMAP_FILE_SUFFIX
+
+    echo "Compare bitmap $output_file1 and $output_file2"
+    cmp $output_file1 $output_file2
     result="$?"
     if [ $result -ne 0 ]; then
         echo "Found differences: $target1, $target2"
@@ -45,6 +70,15 @@ assert trace1 expected
 assert trace2 expected
 assert trace3 expected
 assert trace4 expected
+
+
+# Compare each bitmap
+assert_bitmap trace1 trace2
+assert_bitmap trace1 trace3
+assert_bitmap trace1 trace4
+assert_bitmap trace2 trace3
+assert_bitmap trace2 trace4
+assert_bitmap trace3 trace4
 
 
 # Passed all test cases
--- tests/fib/test.sh
+++ tests/fib/test.sh
@@ -5,13 +5,20 @@
 PROGRAM=../../processor
 # Suffix of the file that outputs edge coverage
 OUTPUT_FILE_SUFFIX="_edge_coverage.out"
+# Suffix of the file that outputs bitmap of edge coverage
+OUTPUT_BITMAP_FILE_SUFFIX="_bitmap.out"
 
 
 # Calculate edge coverage and output to a file
 run () {
     target="$1" # Trace data for calculating edge coverage
     output_file=$target$OUTPUT_FILE_SUFFIX
-    $PROGRAM $(cat $target/decoderargs.txt) > $output_file
+    bitmap_file=$target$OUTPUT_BITMAP_FILE_SUFFIX
+
+    $PROGRAM $(cat $target/decoderargs.txt) --bitmap-mode \
+                                            --bitmap-size=0x1000 \
+                                            --bitmap-filename=$bitmap_file \
+                                            > $output_file
 }
 
 
@@ -23,6 +30,23 @@ assert() {
     output_file2=$target2$OUTPUT_FILE_SUFFIX
 
     diff $output_file1 $output_file2
+    result="$?"
+    if [ $result -ne 0 ]; then
+        echo "Found differences: $target1, $target2"
+        exit 1
+    fi
+}
+
+
+# Compare bitmap
+assert_bitmap() {
+    target1="$1"
+    target2="$2"
+    output_file1=$target1$OUTPUT_BITMAP_FILE_SUFFIX
+    output_file2=$target2$OUTPUT_BITMAP_FILE_SUFFIX
+
+    echo "Compare bitmap $output_file1 and $output_file2"
+    cmp $output_file1 $output_file2
     result="$?"
     if [ $result -ne 0 ]; then
         echo "Found differences: $target1, $target2"
@@ -45,6 +69,15 @@ assert trace1 trace4
 assert trace2 trace3
 assert trace2 trace4
 assert trace3 trace4
+
+
+# Compare each bitmap
+assert_bitmap trace1 trace2
+assert_bitmap trace1 trace3
+assert_bitmap trace1 trace4
+assert_bitmap trace2 trace3
+assert_bitmap trace2 trace4
+assert_bitmap trace3 trace4
 
 
 # Edge coverage matches for multiple trace data for the fib program
