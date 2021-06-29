62163eee74e872e97b4eddece4b70491869a9081
Yuichi Sugiyama <mmxsrup@gmail.com>
2021-06-29 18:54:03 +0900

    src: Update command line argument reading

--- src/processor.cpp
+++ src/processor.cpp
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <cstring>
 
+#include <linux/limits.h>
+
 #include "process.hpp"
 #include "decoder.hpp"
 #include "deformatter.hpp"
@@ -22,12 +24,13 @@ int main(int argc, char const *argv[])
                   << "[binary_data1_filename] [binary_data1_start_address] [binary_data1_end_address] ... "
                   << "[binary_dataN_filename] [binary_dataN_start_address] [binary_dataN_end_address] [OPTIONS]" << std::endl
                   << "OPTIONS:" << std::endl
-                  << "\t--raw_address_mode     : Use raw address as output for edge coverage, not offset in binary." << std::endl
-                  << "\t--address-range=L,R    : Specify the range of addresses to be saved as edge coverage." << std::endl
-                  << "\t                         L and R are hexadecimal values, and the address range is [l, r]." << std::endl
-                  << "\t--bitmap-mode          : Enable bitmap calculation." << std::endl
-                  << "\t--bitmap-size=size     : Specify the bitmap size in hexadecimal. The default size is 0x10000." << std::endl
-                  << "\t--bitmap-filename=name : Specify the file name to save the bitmap. The default name is edge_coverage_bitmap.out"
+                  << "\t--trace-binary-filename=name : Specifies the name of the binary file to trace." << std::endl
+                  << "\t                               This option may be used multiple times to specify multiple binary files." << std::endl
+                  << "\t--bitmap-mode                : Enable bitmap calculation." << std::endl
+                  << "\t--bitmap-size=size           : Specify the bitmap size in hexadecimal. The default size is 0x10000." << std::endl
+                  << "\t--bitmap-filename=name       : Specify the file name to save the bitmap. The default name is edge_coverage_bitmap.out" << std::endl
+                  << "\t--cache-mode                 : Enable cache mode. This mode speeds up the decoding process by saving the disassemble" << std::endl
+                  << "\t                               and trace results in the software cache."
                   << std::endl;
         std::exit(1);
     }
@@ -53,12 +56,36 @@ int main(int argc, char const *argv[])
     const std::vector<uint8_t> trace_data = readBinaryFile(trace_data_filename);
     const std::vector<uint8_t> deformat_trace_data = deformatTraceData(trace_data, trace_id);
 
-    // Read binary data and entry point
-    std::vector<MemoryMap> memory_maps; std::unordered_map<std::string, std::vector<std::uint8_t>> binary_files; {
+    // Read options
+    bool bitmap_mode = false;
+    uint64_t bitmap_size = BITMAP_SIZE;
+    std::string bitmap_filename = BITMAP_FILENAME;
+    std::vector<std::string> trace_binary_filenames;
+    bool cache_mode = false;
+    for (int i = binary_file_num * 3 + 4; i < argc; ++i) {
+        size_t size;
+        char buf[PATH_MAX];
+        if (sscanf(argv[i], "--trace-binary-filename=%s", buf)) {
+            trace_binary_filenames.emplace_back(std::string(buf));
+        } else if (strcmp(argv[i], "--bitmap-mode") == 0) {
+            bitmap_mode = true;
+        } else if (sscanf(argv[i], "--bitmap-size=%lx", &size) == 1) {
+            // TODO: Check if it is an invalid size
+            bitmap_size = size;
+        } else if (sscanf(argv[i], "--bitmap-filename=%s", buf) == 1) {
+            bitmap_filename = std::string(buf);
+        } else if (strcmp(argv[i], "--cache-mode") == 0) {
+            cache_mode = true;
+        } else {
+            std::cerr << "Invalid option: " << argv[i] << std::endl;
+            std::exit(1);
+        }
+    }
+
+    std::vector<MemoryMap> memory_maps; {
         for (int i = 0; i < binary_file_num; i++) {
             // Read binary data
             const std::string binary_data_filename = argv[4 + i * 3];
-            const std::vector<uint8_t> data = readBinaryFile(binary_data_filename);
 
             // Read start/end address
             const std::uint64_t start_address = std::stol(argv[4 + i * 3 + 1], nullptr, 16);
@@ -71,40 +98,13 @@ int main(int argc, char const *argv[])
                     end_address,
                 }
             );
-
-            binary_files.insert(std::make_pair(binary_data_filename, data));
         }
     }
 
-    // Read options
-    bool raw_address_mode = false;
-    uint64_t lower_address_range = 0x0;
-    uint64_t upper_address_range = UINT64_MAX;
-    bool bitmap_mode = false;
-    uint64_t bitmap_size = BITMAP_SIZE;
-    std::string bitmap_filename = BITMAP_FILENAME;
-    bool cache_mode = false;
-    for (int i = binary_file_num * 3 + 4; i < argc; ++i) {
-        uint64_t t1 = 0, t2 = 0;
-        size_t size;
-        char buf[256];
-        if (strcmp(argv[i], "--raw-address-mode") == 0) {
-            raw_address_mode = true;
-        } else if (sscanf(argv[i], "--address-range=%lx,%lx", &t1, &t2) == 2) {
-            lower_address_range = t1;
-            upper_address_range = t2;
-        } else if (strcmp(argv[i], "--bitmap-mode") == 0) {
-            bitmap_mode = true;
-        } else if (sscanf(argv[i], "--bitmap-size=%lx", &size) == 1) {
-            // TODO: Check if it is an invalid size
-            bitmap_size = size;
-        } else if (sscanf(argv[i], "--bitmap-filename=%s", buf)) {
-            bitmap_filename = std::string(buf);
-        } else if (strcmp(argv[i], "--cache-mode") == 0) {
-            cache_mode = true;
-        } else {
-            std::cerr << "Invalid option: " << argv[i] << std::endl;
-            std::exit(1);
+    std::unordered_map<std::string, std::vector<std::uint8_t>> binary_files; {
+        for (const std::string &filename : trace_binary_filenames) {
+            const std::vector<uint8_t> data = readBinaryFile(filename);
+            binary_files.insert(std::make_pair(filename, data));
         }
     }
 
@@ -121,7 +121,7 @@ int main(int argc, char const *argv[])
     // Calculate edge coverage from trace data and binary data
     const ProcessResult result = process(param, deformat_trace_data, memory_maps, handle);
     if (result.type != PROCESS_SUCCESS) {
-        std::cerr << "" << std::endl;
+        std::cerr << "Failed to run process()." << std::endl;
         std::exit(1);
     }
 
